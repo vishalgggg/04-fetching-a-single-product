e3e5d5b9519364537d71f587c347cb3ae4a28cb4
vishal <vsoftjob184@gmail.com>
2024-11-15 09:43:25 -0800

    Error: Client does not support authentication protocol requested by server; consider upgrading MySQL client error

--- util/database.js
+++ util/database.js
@@ -1,10 +1,47 @@
+// const mysql = require('mysql2');
+
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'ecom',
+//     password: '0000'
+// });
+
+// module.exports = pool.promise();
 const mysql = require('mysql2');
-
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'nodecomplete'
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'yourusername',
+  password: 'yourpassword',
+  database: 'yourdatabase',
+  insecureAuth: true
 });
-
 module.exports = pool.promise();
+// const mysql = require('mysql2/promise');
+
+// const connection = await mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '0000',
+//   database: 'ecom',
+//   port: 3306,
+//   authPlugins: {
+//     mysql_native_password: async () => {
+//       return await mysql2.auth.auth(connection, { user: 'root', password: '0000' });
+//     },
+//   },
+// });
+// const mysql = require('mysql2/promise');
+
+// const pool = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '0000',
+//   database: 'ecom',
+//   port: 3306,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
+
+// module.exports = pool;
